bf5dfac18e43ce039588f3e5c590b57142825e8c
Subhadeep Dey <contact.sdey@gmail.com>
2018-04-22 11:25:52 +0530

    Modify log-analyser.py

--- log-analyser.py
+++ log-analyser.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 import psycopg2
 
+DBNAME = "news"
 
-def log_analyzer(self):
-    pass
+
+def execute_query(query):
+    """Connect to the database and execute the query passed to it.
+
+    Argument:
+    query (string) -- The query to execute.
+    """
+    try:
+        conn = psycopg2.connect("dbname=" + DBNAME)
+    except psycopg2.Error as e:
+        print("Unable to connect to the database.")
