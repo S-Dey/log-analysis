7a8b1fe6e44b548df53e678b8ef23c80bc9c7057
Subhadeep Dey <contact.sdey@gmail.com>
2018-04-22 15:51:19 +0530

    Add log analyser (Question 3)

--- log-analyser.py
+++ log-analyser.py
@@ -43,14 +43,81 @@ def get_popular_articles():
     results = execute_query(query)
 
     # Print the result.
-    print('--------------------------------------------------------------------')
-    print('\t\tI. TOP THREE ARTICLES OF ALL TIME')
-    print('--------------------------------------------------------------------')
+    print("----------------------------------------------------------------")
+    print("\t\tI. TOP THREE ARTICLES OF ALL TIME")
+    print("----------------------------------------------------------------")
     rank = 1
     for row in results:
-        print(u"  {0}. \"{1}\" — {2} views.".format(rank, row[0], row[1]))
+        print(u"  {0}. \"{1}\" — {2:,} views.".format(rank, row[0], row[1]))
         rank += 1
+
+
+# --------------------------------------------------------------
+# 2. POPULAR AUTHORS OF ALL TIME
+# --------------------------------------------------------------
+def get_popular_authors():
+    """Return authors sorted by page views."""
+
+    query = """
+        SELECT authors.name, COUNT(*) AS num
+        FROM authors JOIN articles
+        ON authors.id = articles.author
+        JOIN log
+        ON log.path = concat('/article/', articles.slug)
+        GROUP BY authors.name
+        ORDER BY num DESC
+    """
+
+    # Run Query
+    results = execute_query(query)
+
+    # Print Results
+    print("\n\n----------------------------------------------------------------")
+    print("\t\tII. POPULAR AUTHORS OF ALL TIME")
+    print("----------------------------------------------------------------")
+    rank = 1
+    for row in results:
+        print(u"  {0}. {1} — {2:,} views.".format(rank, row[0], row[1]))
+        rank += 1
+
+
+def get_days_with_errors():
+    """returns days with more than 1% errors"""
+
+    # Build Query String
+    query = """
+        SELECT total.day,
+          ROUND(((errors.error_requests*1.0) / total.requests), 3) AS percent
+        FROM (
+          SELECT date_trunc('day', time) "day", count(*) AS error_requests
+          FROM log
+          WHERE status LIKE '404%'
+          GROUP BY day
+        ) AS errors
+        JOIN (
+          SELECT date_trunc('day', time) "day", count(*) AS requests
+          FROM log
+          GROUP BY day
+          ) AS total
+        ON total.day = errors.day
+        WHERE (ROUND(((errors.error_requests*1.0) / total.requests), 3) > 0.01)
+        ORDER BY percent DESC;
+    """
+
+    # Run Query
+    results = execute_query(query)
+
+    # Print Results
+    print("\n\n----------------------------------------------------------------")
+    print("   III. DAYS IN WHICH MORE THAN 1% OF REQUESTS LEAD TO ERRORS")
+    print("----------------------------------------------------------------")
+    for row in results:
+        date = row[0].strftime('%B %d, %Y')         # Pretty-formatting date.
+        errors = str(round(row[1]*100, 1)) + "%" + " errors"
+        print("   " + date + u" — " + errors)
 
 
 if __name__ == '__main__':
     get_popular_articles()
+    get_popular_authors()
+    get_days_with_errors()
